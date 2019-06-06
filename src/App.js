15de6434cea1e903e87db0dd506724955d2e59f7
Kuba <kubaz9991@gmail.com>
2019-06-06 20:06:36 +0200

    sample button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import Layout from './containers/Layout/Layout'
+import Layout from "./containers/Layout/Layout";
+import React from "react";
+import Button from "@material-ui/core/Button";
+
 const App = () => {
   return (
-    <div>
+    <div className='App'>
+      <h1>Hello world!</h1>
       <Layout />
+      <Button variant='contained' color='primary'>
+        Hello World
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
