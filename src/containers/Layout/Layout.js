45289cd57cb0d861995f324ae8ca802db299cabf
xdorijanx <dorijan11@gmail.com>
2019-06-06 19:05:06 +0200

    Added a fixed bottom navigation

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React, { Component, Fragment} from 'react';
 import NavigationTop from '../../components/UI/NavigationTop/NavigationTop';
+import NavigationBottom from '../../components/UI/NavigationBottom/NavigationBottom';
+
 
  class Layout extends Component {
     render() {
         return (
              <Fragment>
                  <NavigationTop />
+                 <NavigationBottom />
              </Fragment>
         )
     }
