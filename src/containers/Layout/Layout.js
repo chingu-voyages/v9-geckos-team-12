8eda82a70cbf0da590790395c32e226ca58b2e06
[Dorijan] <[dorijan11@gmail.com]>
2019-06-06 18:27:33 +0200

    Implemented Top Navigation

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment} from 'react';
+import NavigationTop from '../../components/UI/NavigationTop/NavigationTop';
+
+ class Layout extends Component {
+    render() {
+        return (
+             <Fragment>
+                 <NavigationTop />
+             </Fragment>
+        )
+    }
+}
+
+
+
+export default Layout
