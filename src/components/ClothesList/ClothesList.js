0c2b80b9794fe6928a95cbe8292bc579428b80a2
Kuba <kubaz9991@gmail.com>
2019-06-08 19:48:15 +0200

    cards

--- src/components/ClothesList/ClothesList.js
+++ src/components/ClothesList/ClothesList.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Card from "../ClothesList/Card/Card";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
 const config = {
   headers: {
     "X-RapidAPI-Key": "07ea04e210mshdcdcdfaeb0a8a2fp1b7079jsnb16592877577"
   }
 };
+
 export default class ClothesList extends Component {
   constructor(props) {
     super(props);
@@ -22,12 +27,21 @@ export default class ClothesList extends Component {
       .then(res => this.setState({ items: res.data.products }));
   }
   render() {
+    const items = this.state.items;
+    console.log(items);
     return (
       <div>
-        <p>ClothesList</p>
-        {this.state.items.map(item => {
-          return <li key={item.id}>{item.name}</li>;
-        })}
+        <Grid container>
+          {this.state.items.map(item => {
+            return (
+              <Card
+                name={item.name}
+                img={item.baseImageUrl}
+                price={item.price.current.text}
+              />
+            );
+          })}
+        </Grid>
       </div>
     );
   }
