1469e9b5a2bb74e3f30d33486f5d98baca5d1595
[Dorijan] <[dorijan11@gmail.com]>
2019-06-09 18:22:21 +0200

    Updates

---
+++ precache-manifest.013861a09753d9cd5349dda4d0f24709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b41e9008e65c49cd90695b0edc66e1",
+    "url": "/v9-geckos-team-12/index.html"
+  },
+  {
+    "revision": "049ee487f27300ee288d",
+    "url": "/v9-geckos-team-12/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "e4346453003a0a1d47b0",
+    "url": "/v9-geckos-team-12/static/js/2.5a662d76.chunk.js"
+  },
+  {
+    "revision": "049ee487f27300ee288d",
+    "url": "/v9-geckos-team-12/static/js/main.caf013ab.chunk.js"
+  },
+  {
+    "revision": "63f500283d08964391e6",
+    "url": "/v9-geckos-team-12/static/js/runtime~main.b5aaf299.js"
+  }
+]);
