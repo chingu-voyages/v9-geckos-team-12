b1ec0babd68f74c6429bbedbe854bcfea4a9d441
Kuba <kubaz9991@gmail.com>
2019-06-13 17:44:34 +0200

    fixed rendering

--- src/components/ClothesList/ClothesList.js
+++ src/components/ClothesList/ClothesList.js
@@ -16,22 +16,45 @@ export default class ClothesList extends Component {
     super(props);
     this.state = {
       items: [],
-      searchTerm: "t-shirt"
+      params: this.props.location.pathname.slice(1)
     };
   }
-
+  //fetch for the first time
   componentDidMount() {
-    let searchTerm = this.state.searchTerm;
+    let params = this.state.params;
     axios
       .get(
-        `https://brianiswu-unofficial-asos-com-v1.p.rapidapi.com/product/search/v1/?q=${searchTerm}&lang=en-GB&currency=EUR&store=1`,
+        `https://brianiswu-unofficial-asos-com-v1.p.rapidapi.com/product/search/v1/?q=${params}&lang=en-GB&currency=EUR&store=1`,
         config
       )
       .then(res => this.setState({ items: res.data.products }));
   }
+
+  componentDidUpdate(prevState) {
+    let params = this.props.location.pathname.slice(1);
+    let currentData;
+    console.log(this.props);
+    console.log(params);
+    axios
+      .get(
+        `https://brianiswu-unofficial-asos-com-v1.p.rapidapi.com/product/search/v1/?q=${params}&lang=en-GB&currency=EUR&store=1`,
+        config
+      )
+      .then(res => {
+        currentData = res.data.products;
+
+        if (prevState.items !== currentData) {
+          this.setState({
+            items: currentData
+          });
+        }
+      });
+  }
+
+  fetchClothes = () => {};
+
   render() {
     const items = this.state.items;
-    console.log(items);
     return (
       <div>
         <Grid container>
