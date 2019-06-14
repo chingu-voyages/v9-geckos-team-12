e6d1d6873c8904d8de2d57f726381ab563cbad29
[Dorijan] <[dorijan11@gmail.com]>
2019-06-14 16:20:28 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/v9-geckos-team-12/precache-manifest.234d339d39cc674bf53c6e499f1e66a9.js"
+  "/v9-geckos-team-12/precache-manifest.ef1a1560176be32446a82514eee2cd69.js"
 );
 
 self.addEventListener('message', (event) => {
