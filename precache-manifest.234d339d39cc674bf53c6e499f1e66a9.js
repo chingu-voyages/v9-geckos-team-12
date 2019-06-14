38bbacafef0e5ca57d87dccf7efa375d306d4e85
[Dorijan] <[dorijan11@gmail.com]>
2019-06-14 16:24:21 +0200

    Updates

--- precache-manifest.234d339d39cc674bf53c6e499f1e66a9.js
+++ precache-manifest.234d339d39cc674bf53c6e499f1e66a9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4e826ddb5b64736a9f0d5b9c8884987a",
+    "revision": "4821316fb997dfdf5683b589aa17084f",
     "url": "/v9-geckos-team-12/index.html"
   },
   {
-    "revision": "4e1eb58c0de33d4efdf5",
+    "revision": "3cafff03d28f9b7cbc0a",
     "url": "/v9-geckos-team-12/static/css/main.498d002a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/v9-geckos-team-12/static/js/2.b8b339bd.chunk.js"
   },
   {
-    "revision": "4e1eb58c0de33d4efdf5",
-    "url": "/v9-geckos-team-12/static/js/main.2b2d0a9f.chunk.js"
+    "revision": "3cafff03d28f9b7cbc0a",
+    "url": "/v9-geckos-team-12/static/js/main.e9b36545.chunk.js"
   },
   {
     "revision": "63f500283d08964391e6",
