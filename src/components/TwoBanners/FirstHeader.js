faa21931be7f2aca87b873f174c7d99b208c79a5
LightingSpace <[debianuk@yahoo.co.uk]>
2019-06-16 14:34:34 +0200

    first commit -adding changes files -should work

---
+++ src/components/TwoBanners/FirstHeader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import "../../css/FirstHeader.css";
+import InnerBox from "../../components/TwoBanners/InnerBox";
+
+let FirstHeader = props => {
+  return (
+    <Box className="forAbsolute">
+      <Box className="pictureClass" />
+      <InnerBox
+        namehone="NEWS !!!!"
+        namesecond="Check Now !!!"
+        linkmoredetails="More Details"
+      />
+    </Box>
+  );
+};
+
+export default FirstHeader;
