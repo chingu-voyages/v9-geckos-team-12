58c22703686842c7d944c655819ac930a3a99102
LightingSpace <[debianuk@yahoo.co.uk]>
2019-06-16 14:34:34 +0200

    first commit -adding changes files -should work

--- src/components/UI/NavigationBottom/NavigationBottom.js
+++ src/components/UI/NavigationBottom/NavigationBottom.js
@@ -1,46 +1,52 @@
-import React from 'react'
-import Box from '@material-ui/core/Box';
+import React from "react";
+import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
-
 const useStyles = makeStyles({
-    root: {
-      flexGrow: 1
-    },
-    appBar: {
-        top: 'auto',
-        bottom: 0,
-      },
-  });
+  root: {
+    flexGrow: 1
+  },
+  appBar: {
+    top: "auto",
+    bottom: 0
+  }
+});
 
 const NavigationBottom = () => {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="fixed" color="primary" className={classes.appBar}>
+        <Toolbar style={{ alignSelf: "center" }}>
+          <Box p={2}>
+            <Typography
+              variant="button"
+              color="inherit"
+              style={{ borderRight: "0.1em solid ", paddingRight: "33px" }}
+            >
+              Our Catalog
+            </Typography>
+          </Box>
+          <Box p={2}>
+            <Typography
+              variant="button"
+              color="inherit"
+              style={{ borderRight: "0.1em solid ", paddingRight: "33px" }}
+            >
+              Terms & Conditions
+            </Typography>
+          </Box>
+          <Box p={2}>
+            <Typography variant="button" color="inherit">
+              Privacy
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
-    return (
-        <div>
-             <AppBar position="fixed" color="primary" className={classes.appBar}>
-                 <Toolbar style={{alignSelf: 'center'}}>
-                    <Box p={2}>
-                        <Typography variant="button" color="inherit"   style={{ borderRight: '0.1em solid ', paddingRight: '33px' }}>
-                            Our Catalog 
-                        </Typography>
-                        </Box>
-                        <Box p={2}>
-                        <Typography variant="button" color="inherit" style={{ borderRight: '0.1em solid ', paddingRight: '33px' }}>
-                              Terms & Conditions 
-                        </Typography>
-                        </Box>
-                        <Box p={2}>
-                        <Typography variant="button" color="inherit">
-                             Privacy
-                        </Typography>
-                        </Box>
-                    </Toolbar>
-             </AppBar>
-        </div>
-    )
-}
-
-export default NavigationBottom
+export default NavigationBottom;
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,14 +1,36 @@
 import React, { Component, Fragment } from "react";
 import NavigationTop from "../../components/UI/NavigationTop/NavigationTop";
 import NavigationBottom from "../../components/UI/NavigationBottom/NavigationBottom";
+import BigCollectEmails from "../../components/CollectEmails/BigCollectEmails";
+import FirstHeader from "../../components/TwoBanners/FirstHeader";
+import SecondHeader from "../../components/TwoBanners/SecondHeader";
 
 class Layout extends Component {
   render() {
     return (
-      <Fragment>
+      /* <Fragment>
         <NavigationTop />
         {this.props.children}
         <NavigationBottom />
+      </Fragment> */
+      <Fragment>
+        <Fragment>
+          <NavigationTop />
+        </Fragment>
+        <Fragment>
+          <FirstHeader />
+        </Fragment>
+
+        <Fragment>{this.props.children}</Fragment>
+        <Fragment>
+          <SecondHeader />
+        </Fragment>
+        <Fragment>
+          <BigCollectEmails />
+        </Fragment>
+        <Fragment>
+          <NavigationBottom />
+        </Fragment>
       </Fragment>
     );
   }
