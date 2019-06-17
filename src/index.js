5e0529e15f862528d1d1e3f3930ab888e6d227df
Kuba <kubaz9991@gmail.com>
2019-06-17 22:13:23 +0200

    redux compose fix

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ import { Provider } from "react-redux";
 import rootReducer from "./reducers/rootReducer";
 import thunk from "redux-thunk";
 
+let devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production") {
+  devTools = a => a;
+}
+
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
 
