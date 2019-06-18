6cc761e1bfe5172f257c8ebeabffe6a919bf08eb
Kuba <kubaz9991@gmail.com>
2019-06-18 21:03:28 +0200

    Details page and link created

--- src/components/ClothesList/Card/Card.js
+++ src/components/ClothesList/Card/Card.js
@@ -7,12 +7,13 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   card: {
     maxWidth: 345,
-    marginTop: '20px',
-    marginBottom: '20px'
+    marginTop: "20px",
+    marginBottom: "20px"
   },
   media: {
     height: 420
@@ -24,17 +25,20 @@ export default function CardComponent({ ...props }) {
 
   return (
     <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia className={classes.media} image={props.img} />
-        <CardContent>
-          <Typography gutterBottom variant='caption' component='h2'>
-            {props.name}
-          </Typography>
-          <Typography variant='body2' color='textSecondary' component='p'>
-            Price: {props.price}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link to='/details'>
+        <CardActionArea>
+          <CardMedia className={classes.media} image={props.img} />
+          <CardContent>
+            <Typography gutterBottom variant='caption' component='h2'>
+              {props.name}
+            </Typography>
+            <Typography variant='body2' color='textSecondary' component='p'>
+              Price: {props.price}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
+
       <CardActions>
         <Button size='small' color='primary'>
           ADD TO BASKET
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from "react";
 import NavigationTop from "../../components/UI/NavigationTop/NavigationTop";
 import NavigationBottom from "../../components/UI/NavigationBottom/NavigationBottom";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import ClothesList from "../../components/ClothesList/ClothesList";
+import Details from "../../components/Details/Details";
 // Layout will render NavigationTop, NavigationBottom,ClothesList
 
 class Layout extends Component {
@@ -17,7 +18,13 @@ class Layout extends Component {
     return (
       <Fragment>
         <NavigationTop />
-        <Route component={ClothesList} path='/' />
+        <Switch>
+          <Route component={ClothesList} path='/jeans' />
+          <Route component={ClothesList} path='/t-shirts' />
+          <Route component={ClothesList} path='/shoes' />
+
+          <Route component={Details} path='/details' exact />
+        </Switch>
         <NavigationBottom />
       </Fragment>
     );
