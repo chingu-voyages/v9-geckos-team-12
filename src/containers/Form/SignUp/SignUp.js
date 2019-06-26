d3651cffa3181952cf9e2560da836de2266c59c6
[Dorijan] <[dorijan11@gmail.com]>
2019-06-26 18:31:27 +0200

    Changed typography

--- src/containers/Form/SignUp/SignUp.js
+++ src/containers/Form/SignUp/SignUp.js
@@ -1,58 +1,64 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import { connect } from "react-redux";
-import { signInAction } from '../../../actions/formActions'
+import { signInAction } from "../../../actions/formActions";
+import { Fragment } from "react";
 
 function MadeWithLove() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Built with love by the '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Material-UI
-      </Link>
-      {' team.'}
-    </Typography>
+    <Fragment>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Built with love by the "}
+        <Link color="inherit" href="https://material-ui.com/">
+          Material-UI
+        </Link>
+        {" team. "}
+      </Typography>
+      <Typography variant="body2" color="textSecondary" align="center">
+        Edited to suit this project by Chingu Geckos-12
+      </Typography>
+    </Fragment>
   );
 }
 
 const useStyles = makeStyles(theme => ({
-  '@global': {
+  "@global": {
     body: {
-      backgroundColor: theme.palette.common.white,
-    },
+      backgroundColor: theme.palette.common.white
+    }
   },
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
-const SignUp = (props) =>  {
+const SignUp = props => {
   const classes = useStyles();
 
   return (
@@ -131,7 +137,7 @@ const SignUp = (props) =>  {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-            <Link
+              <Link
                 href="#"
                 variant="body2"
                 onClick={() => {
@@ -149,12 +155,11 @@ const SignUp = (props) =>  {
       </Box>
     </Container>
   );
-}
+};
 
 const mapStateToProps = state => ({
-    signUp: state.form.signUp,
-    signIn: state.form.signIn
-  });
-  
-  export default connect(mapStateToProps)(SignUp);
-  
+  signUp: state.form.signUp,
+  signIn: state.form.signIn
+});
+
+export default connect(mapStateToProps)(SignUp);
