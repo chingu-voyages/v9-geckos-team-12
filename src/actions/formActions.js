8326f19abf715ed2340dd09029acbda6b6828e22
[Dorijan] <[dorijan11@gmail.com]>
2019-06-26 16:15:28 +0200

    Implemented redux store reducer and action

---
+++ src/actions/formActions.js
@@ -0,0 +1,10 @@
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_UP = 'SIGN_UP';
+
+export const signIn = () => ({
+    type: SIGN_IN
+})
+
+export const signUp = () => ({
+    type: SIGN_UP
+})
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import products from "./productReducer";
-
+import form from './formReducer'
 export default combineReducers({
-  products
+  products, form
 });
