6689355414cdca47e42060fd55aa8f792e6476dd
xdorijanx <dorijan11@gmail.com>
2019-06-26 15:28:19 +0200

    implemented SignIn form

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -4,7 +4,7 @@ import NavigationBottom from "../../components/UI/NavigationBottom/NavigationBot
 import { Route, Switch } from "react-router-dom";
 import ClothesList from "../../components/ClothesList/ClothesList";
 import Details from "../../components/Details/Details";
-import Form from '../Form/Form'
+import SignIn from '../Form/SignIn/SignIn'
 // Layout will render NavigationTop, NavigationBottom,ClothesList
 
 class Layout extends Component {
@@ -18,7 +18,7 @@ class Layout extends Component {
           <Route component={ClothesList} path='/shoes' />
 
           <Route component={Details} path='/details' exact />
-          <Route render={() => <Form />} path='/login'  />
+          <Route component={SignIn} path='/login'  />
         </Switch>
         <NavigationBottom />
       </Fragment>
