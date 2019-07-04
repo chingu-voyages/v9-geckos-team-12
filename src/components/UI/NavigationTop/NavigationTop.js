310ea4937912620bf718e55099ee1722c93b497b
[Dorijan] <[dorijan11@gmail.com]>
2019-07-04 22:08:57 +0200

    Implemented all items for each dropdown menu and their actions

--- src/components/UI/NavigationTop/NavigationTop.js
+++ src/components/UI/NavigationTop/NavigationTop.js
@@ -1,21 +1,35 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import ResponsiveDrawer from "../SideDrawer/SideDrawer";
-import { AppBar, Toolbar, Button,Menu,MenuItem,List,ListItem,ListItemText } from "@material-ui/core";
+import { AppBar, Toolbar,Typography, Button,Menu,MenuItem,List,ListItem,ListItemText } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import SignIn from '../../../containers/Form/SignIn/SignIn'
 
 import { connect } from "react-redux";
 import { selectCategory } from "../../../actions/productActions";
+import { openMenuAction, openMenuItemAction, menuCloseAction} from '../../../actions/navigationActions';
 
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 const navigationStyles = makeStyles(theme => ({
   list: {
     display: 'flex',
+  
 
+  },
+  paper: {
+      boxShadow: 'none !important',
+      marginTop: 50,
+      width: '90vw'
   }
 }))
+
+const options = {
+  clothing: [`Hoodies & Sweatshits`, `Jackets & Coats`, 'Jeans', 'Shirts', 'Shorts', 'Loungewear', 'Suits', 'Socks', 'Swimwear', 'Vests', 'Trousers', 'Underwear' ],
+  shirts: ['Adidas', 'Nike', 'Puma', 'Reebook', 'Hugo Boss', 'Michael Kors', 'Diesel', 'Lacoste', 'CalvinKlein', 'Nautica'],
+  shoes: ['View All', 'Boots', 'Espadrilles', 'Loafers', 'Sandals',  'Shoes', 'FlipFlops']
+}
+
 
 function NavigationTop(props) {
   let handleSelectCategory = category => {
@@ -45,21 +59,30 @@ function NavigationTop(props) {
         <Toolbar>
           <List component='nav' className={classes.list}>
             <ListItem button>
-              <ListItemText>
-                    Jeans
+              <ListItemText onClick={(e, clothingType) => props.openMenuAction(e.currentTarget, 'clothing')}>
+                    Clothing
               </ListItemText>
-            </ListItem>
-            <ListItem button>
-              <ListItemText>
-                Shirts
-              </ListItemText>
-            </ListItem>
-            <ListItem button>
+            </ListItem >
+           
+            <ListItem button onClick={(e, clothingType) => props.openMenuAction(e.currentTarget, 'shoes')}>
               <ListItemText>
                 Shoes
               </ListItemText>
             </ListItem>
+            <ListItem button onClick={(e, clothingType) => props.openMenuAction(e.currentTarget, 'activewear')}>
+              <ListItemText>
+                Activewear
+              </ListItemText>
+            </ListItem>
           </List>
+          <Menu open={props.clothing || props.shoes || props.activewear} anchorEl={props.anchorEl} elevation={0} className={classes.paper}>
+            {props.items ? props.items.map((option, index) => (
+              <MenuItem key={index} onClick={(e) => props.openMenuItemAction(e.currentTarget)} style={{minHeight: '3px'}}>
+               <Typography variant='caption'> {option}</Typography>
+              </MenuItem>
+            )): null}
+        
+          </Menu>
         </Toolbar>
       </AppBar>
     </div>
@@ -68,11 +91,25 @@ function NavigationTop(props) {
 }
 
 const mapDispatchToProps = {
-  selectCategory
+  selectCategory,
+  openMenuAction,
+  openMenuItemAction,
+  menuCloseAction
 };
 
 const mapStateToProps = state => ({
-  basket: state.products.basket
+  basket: state.products.basket,
+  menuOpen: state.navigation.menuOpen,
+  menuItemOpen: state.navigation.menuItemOpen,
+  anchorEl: state.navigation.anchorEl,
+  anchorItemEl: state.navigation.anchorItemEl,
+  clothingItems: state.navigation.clothingItems,
+  shoesItems: state.navigation.shoesItems,
+  activewearItems: state.navigation.activewearItems,
+  clothing: state.navigation.clothing,
+  shoes: state.navigation.shoes,
+  activewear: state.navigation.activewear,
+  items: state.navigation.items
 });
 
 export default connect(
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import products from "./productReducer";
 import form from './formReducer'
+import navigation from './navigationReducer'
 export default combineReducers({
-  products, form
+  products, form,navigation
 });
