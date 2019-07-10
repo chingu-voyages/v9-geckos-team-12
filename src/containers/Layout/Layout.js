c9d7dd5f1b2b03f33cf683d212053eac08b57698
Kuba <kubaz9991@gmail.com>
2019-07-10 20:26:49 +0200

    breakpoints changed

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -6,32 +6,41 @@ import ClothesList from "../../components/ClothesList/ClothesList";
 import Details from "../../components/Details/Details";
 import Form from "../Form";
 import Basket from "../../components/Basket/Basket";
-import { createMuiTheme,  } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import { teal } from '@material-ui/core/colors';
+import { teal } from "@material-ui/core/colors";
 
 // Layout will render NavigationTop, NavigationBottom,ClothesList
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#a4a4a4',
-      main: '#757575',
-      dark: '#494949',
-      contrastText: '#fff',
+      light: "#a4a4a4",
+      main: "#757575",
+      dark: "#494949",
+      contrastText: "#fff"
     },
     secondary: {
-      light: '#819ca9',
-      main: '#546e7a',
-      dark: '#29434e',
-      contrastText: '#fff',
-    },
-  },  
+      light: "#819ca9",
+      main: "#546e7a",
+      dark: "#29434e",
+      contrastText: "#fff"
+    }
+  },
   overrides: {
     MuiPaper: {
       root: {
         margin: "0 auto"
       }
+    }
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 1024,
+      xl: 1920
     }
   }
 });
@@ -40,19 +49,17 @@ class Layout extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-
-      
-      <Fragment>
-        <NavigationTop />
-        <Switch>
-          <Route component={ClothesList} path='/clothing' />
-          <Route component={Details} path='/details' exact />
-          <Route component={Form} path='/login' exact />
-          <Route component={Basket} path='/basket' exact />
-          <Route component={ClothesList} path='/search/*' />
-        </Switch>
-        <NavigationBottom />
-      </Fragment>
+        <Fragment>
+          <NavigationTop />
+          <Switch>
+            <Route component={ClothesList} path='/clothing' />
+            <Route component={Details} path='/details' exact />
+            <Route component={Form} path='/login' exact />
+            <Route component={Basket} path='/basket' exact />
+            <Route component={ClothesList} path='/search/*' />
+          </Switch>
+          <NavigationBottom />
+        </Fragment>
       </ThemeProvider>
     );
   }
