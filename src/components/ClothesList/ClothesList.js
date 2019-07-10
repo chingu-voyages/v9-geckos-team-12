a9831815e97ca468216e01747371d68d2cb492ed
Kuba <kubaz9991@gmail.com>
2019-07-10 20:26:24 +0200

    added spacing in grid

--- src/components/ClothesList/ClothesList.js
+++ src/components/ClothesList/ClothesList.js
@@ -15,8 +15,6 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import { Button } from "@material-ui/core";
 import SortBy from "../SortBy";
 
-
-
 class ClothesList extends Component {
   //fetch for the first time
   componentDidMount() {
@@ -51,48 +49,45 @@ class ClothesList extends Component {
       return <h1>oops</h1>;
     }
     return (
-
-      
-        <Fragment>
-          {/* sorting menu below */}
-          <SortBy {...this.props} />
-          <Grid container justify='center' alignItems='center'>
-            {products.map(item => {
-              if (item === undefined || null) {
-                return null;
-              } else {
-                return (
-                  <Grid item key={item.id} xs={12} sm={12} md={6} lg={3}>
-                    {" "}
-                    {/* sm indicates how much of the page will an individual card cover xs= exstra small etc..*/}
-                    <Card
-                      key={item.id}
-                      id={item.id}
-                      name={item.name}
-                      img={item.baseImageUrl}
-                      price={item.price.current.text}
-                    />
-                  </Grid>
-                );
-              }
-            })}
-          </Grid>
-          <Button
-            autoFocus={false}
-            variant='contained'
-            color='primary'
-            onClick={() => {
-              this.props.dispatch(updateOffset(40));
-              this.props.dispatch(
-                fetchMoreProducts(this.props.category, 40 + this.props.offset)
+      <Fragment>
+        {/* sorting menu below */}
+        <SortBy {...this.props} />
+        <Grid container justify='center' alignItems='center' spacing={2}>
+          {products.map(item => {
+            if (item === undefined || null) {
+              return null;
+            } else {
+              return (
+                <Grid item key={item.id} xs={6} sm={6} md={4} lg={3}>
+                  {" "}
+                  {/* sm indicates how much of the page will an individual card cover xs= exstra small etc..*/}
+                  <Card
+                    key={item.id}
+                    id={item.id}
+                    name={item.name}
+                    img={item.baseImageUrl}
+                    price={item.price.current.text}
+                  />
+                </Grid>
               );
-            }}
-          >
-            LOAD MORE
-          </Button>
-        </Fragment>
- 
-    )
+            }
+          })}
+        </Grid>
+        <Button
+          autoFocus={false}
+          variant='contained'
+          color='primary'
+          onClick={() => {
+            this.props.dispatch(updateOffset(40));
+            this.props.dispatch(
+              fetchMoreProducts(this.props.category, 40 + this.props.offset)
+            );
+          }}
+        >
+          LOAD MORE
+        </Button>
+      </Fragment>
+    );
   }
 }
 
