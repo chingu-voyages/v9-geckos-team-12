57c22064c82346f4a1d89c52fd1f1c015cfad420
[Dorijan] <[dorijan11@gmail.com]>
2019-07-11 16:58:52 +0200

    Go back buttons ripple effect is removed

--- src/components/UI/NavigationTop/NavigationTop.js
+++ src/components/UI/NavigationTop/NavigationTop.js
@@ -27,52 +27,64 @@ import { selectCategory } from "../../../actions/productActions";
 
 import MenuIcon from "@material-ui/icons/Menu";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import ShoppingCart from '@material-ui/icons/ShoppingCart'
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
 import ArrowBack from "@material-ui/icons/ArrowBack";
 
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 const navigationStyles = makeStyles(theme => ({
   list: {
-   
     display: "flex",
-    [theme.breakpoints.down("xs")]: {
+    
+  },
+  listItem: {
+    marginRight: theme.spacing(6),
+    [theme.breakpoints.down("sm")]: {
       display: "none"
     }
   },
-  listItem: {
-    marginRight: theme.spacing(6)
-  },
 
+  listItemSmall: {
+  
+  },
 
   menuIcon: {
     display: "none",
-    [theme.breakpoints.down("xs")]: {
+    [theme.breakpoints.down("sm")]: {
       display: "inline-block"
     }
   },
   sideDrawer: {
     display: "none",
-    [theme.breakpoints.down("xs")]: {
+    [theme.breakpoints.down("sm")]: {
       display: "flex",
-      width: 450
+      
     }
   },
   sideDrawerList: {
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
+    minWidth: '50vw'
   },
 
   menu: {
-   
-    [theme.breakpoints.down("xs")]: {
+    [theme.breakpoints.down("sm")]: {
       display: "none"
     },
-    backgroundColor: theme.palette.primary.light,
+  
   },
   a: {
-    textDecoration:'none',
-    color: 'inherit'
+    textDecoration: "none",
+    color: "inherit"
+  },
+
+  menuItem: {
+    minWidth: '150px',
+    backgroundColor: theme.palette.secondary
+
+  },
+  label: {
+    justifyContent: 'flex-start'
   }
 }));
 
@@ -113,6 +125,7 @@ function NavigationTop(props) {
 
   const sideDrawerToggle = () => {
     setToggleSideDrawer(!toggleSideDrawer);
+    setAnchorEl(null)
   };
 
   const goBack = () => {
@@ -127,7 +140,11 @@ function NavigationTop(props) {
 
   return (
     <div>
-      <AppBar position='static' onMouseEnter={e => handleClose()} color='primary'>
+      <AppBar
+        position="static"
+        onMouseEnter={e => handleClose()}
+        color="primary"
+      >
         <Toolbar>
           {/* SIDE DRAWER START */}
           <IconButton className={classes.menuIcon} onClick={sideDrawerToggle}>
@@ -139,7 +156,7 @@ function NavigationTop(props) {
                 {menuOption === "activewear" ||
                 menuOption === "footwear" ||
                 menuOption === "clothing" ? (
-                  <IconButton onClick={goBack}>
+                  <IconButton onClick={goBack} classes={{label: classes.label}} disableRipple={true} disableFocusRipple={true}>
                     {" "}
                     <ArrowBack />{" "}
                   </IconButton>
@@ -147,7 +164,7 @@ function NavigationTop(props) {
                 {/* Make sure go back doesn't show unless we're inside of a menu */}
                 {options[menuOption] ? (
                   options[menuOption].map((option, index) => (
-                    <Link to='/clothing' className={classes.a}>
+                    <Link to="/clothing" className={classes.a}>
                       <ListItem
                         button
                         onClick={e => handleSelectCategory(e.target.id)}
@@ -160,41 +177,43 @@ function NavigationTop(props) {
                           {option}
                         </ListItemText>
                       </ListItem>
+                      <Divider />
                     </Link>
                   ))
                 ) : (
                   <Fragment>
+                    
                     <Button
-                      variant='button'
+                      variant="button"
                       onClick={(e, menuOption) => handleClick(e, "clothing")}
                     >
                       Clothing
                     </Button>
-
+                    <Divider />
                     <Button
-                      variant='button'
+                      variant="button"
                       onClick={(e, menuOption) => handleClick(e, "footwear")}
                     >
                       Footwear
                     </Button>
-
-                    <Link to='/clothing' className={classes.a}>
+                    <Divider />
+                  
                       <Button
-                        variant='button'
+                        variant="button"
                         onClick={(e, menuOption) =>
                           handleClick(e, "activewear")
                         }
                       >
                         Activewear
                       </Button>
-                    </Link>
+                  
                   </Fragment>
                 )}
               </List>
             </SwipeableDrawer>
           </div>
           {/* SIDE DRAWER END */}
-          <List component='nav' className={classes.list}>
+          <List component="nav" className={classes.list}>
             <ListItem
               autoFocus={Boolean(anchorEl)}
               button
@@ -230,12 +249,12 @@ function NavigationTop(props) {
               <SearchBar />
             </ListItem>
             <ListItem button>
-              <Link to='/basket' replace className={classes.a}>
-              <IconButton aria-label="Show 4 new mails" color="inherit">
-          <Badge badgeContent={props.basket.length} color="secondary">
-            <ShoppingCart />
-          </Badge>
-        </IconButton>
+              <Link to="/basket" replace className={classes.a}>
+                <IconButton aria-label="Show 4 new mails" color="inherit">
+                  <Badge badgeContent={props.basket.length} color="secondary">
+                    <ShoppingCart />
+                  </Badge>
+                </IconButton>
               </Link>
             </ListItem>
             <ListItem button>
@@ -260,8 +279,9 @@ function NavigationTop(props) {
                 menuOption
               ] /* Using ternary operator to make sure items are available otherwise we get an error because items are initially null */
                 ? options[menuOption].map((option, index) => (
-                    <Link to='/clothing' className={classes.a }>
+                    <Link to="/clothing" className={classes.a}>
                       <MenuItem
+                        className={classes.menuItem}
                         onClick={e => {
                           handleSelectCategory(e.target.id);
                           handleClose();
@@ -271,7 +291,7 @@ function NavigationTop(props) {
                         style={{ minHeight: "3px" }}
                       >
                         <Typography
-                          variant='caption'
+                          variant="caption"
                           style={{ pointerEvents: "none" }}
                         >
                           {" "}
