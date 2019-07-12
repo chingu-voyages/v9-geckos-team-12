c83b2fb633f744d09954ca67b87b55e94f1e2c4e
Kuba <kubaz9991@gmail.com>
2019-07-12 18:44:01 +0200

    grid in basket

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -4,27 +4,15 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import { Button } from "@material-ui/core";
 import { removeItemFromBasket } from "../../actions/productActions";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  basketItems: {
-    flexGrow: "2"
-  },
-  basketSummary: {
-    flexGrow: "1",
-    margin: theme.spacing(2, 2)
-  },
-  basketItem: {
-    padding: theme.spacing(3, 2),
-    margin: theme.spacing(2, 2)
-  },
   list: {
     listStyleType: "none"
   },
   thumbnail: {
-    width: "10%"
+    width: "8%"
   }
 }));
 const Basket = props => {
@@ -38,50 +26,73 @@ const Basket = props => {
   return (
     <React.Fragment>
       <div className={classes.root}>
-        <div className={classes.basketItems}>
-          <ul className={classes.list}>
-            {basket.map(item => {
-              return (
-                <li key={item.id}>
-                  <Paper className={classes.basketItem}>
-                    <img
-                      className={classes.thumbnail}
-                      src={`https://${item.media.images[0].url}`}
-                      alt='thumbnail'
-                    />
-                    {item.name}
-                    {item.price.current.text}
-                    {item.isInStock ? "IN STOCK" : null}
-                    <Button
-                      variant='contained'
-                      color='primary'
-                      onClick={id =>
-                        props.dispatch(removeItemFromBasket(item.id))
-                      }
-                    >
-                      REMOVE
-                    </Button>
-                  </Paper>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div className={classes.basketSummary}>
-          <Paper>
-            <h1>Order summary:</h1>
-            <h4>Total cost: {totalCost.toFixed(2)} EUR</h4>
-            {totalCost <= 0 ? null : (
-              <Button
-                variant='contained'
-                color='primary'
-                onClick={() => alert("Redirecting to checkout")}
+        <Grid
+          container
+          direction='row'
+          justify='center'
+          alignItems='flex-start'
+          spacing={3}
+        >
+          <Grid item md={9} lg={9} xl={9}>
+            <ul className={classes.list}>
+              <Grid
+                container
+                direction='column'
+                justify='space-around'
+                alignItems='stretch'
+                spacing={3}
               >
-                PROCEED TO CHECKOUT
-              </Button>
-            )}
-          </Paper>
-        </div>
+                {basket.map(item => {
+                  return (
+                    <Grid item>
+                      <li key={item.id}>
+                        <Paper className={classes.basketItem}>
+                          <Card>
+                            <img
+                              className={classes.thumbnail}
+                              src={`https://${item.media.images[0].url}`}
+                              alt='thumbnail'
+                            />
+                            {item.name}
+                            {item.price.current.text}
+                            {item.isInStock ? "IN STOCK" : null}
+                            <Button
+                              variant='contained'
+                              color='primary'
+                              onClick={id =>
+                                props.dispatch(removeItemFromBasket(item.id))
+                              }
+                            >
+                              REMOVE
+                            </Button>
+                          </Card>
+                        </Paper>
+                      </li>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </ul>
+          </Grid>
+
+          <Grid item md={3} lg={3} xl={3}>
+            <div className={classes.basketSummary}>
+              <Paper>
+                <h1>Order summary:</h1>
+                <h4>Total cost: {totalCost.toFixed(2)} EUR</h4>
+                {totalCost <= 0 ? null : (
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    onClick={() => alert("Redirecting to checkout")}
+                  >
+                    PROCEED TO CHECKOUT
+                  </Button>
+                )}
+              </Paper>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     </React.Fragment>
   );
