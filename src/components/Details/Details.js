31a55a2faaab3f4e1f5fe61b065036080ddf52c1
[Dorijan] <[dorijan11@gmail.com]>
2019-07-14 00:55:11 +0200

    Fixed layout breaking on extra small screens

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,12 +3,11 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import { Slide } from "react-slideshow-image";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Grid } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-
 import {
   fetchProductByID,
   addItemToBasket
@@ -63,11 +62,7 @@ const Details = props => {
   }
 
   if (loading) {
-    return (
-      <Grid container justify='center' style={{ marginTop: "400px" }}>
-        <CircularProgress className='progress' color='secondary' size={80} />{" "}
-      </Grid>
-    );
+    return <CircularProgress className='progress' color='secondary' />;
   }
 
   return (
--- src/components/UI/NavigationBottom/NavigationBottom.js
+++ src/components/UI/NavigationBottom/NavigationBottom.js
@@ -3,6 +3,7 @@ import Box from '@material-ui/core/Box';
 import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Typography, useScrollTrigger } from "@material-ui/core";
 import Slide from '@material-ui/core/Slide';
+import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles({
     root: {
@@ -11,6 +12,10 @@ const useStyles = makeStyles({
     appBar: {
         top: 'auto',
         bottom: 0,
+      },
+      a: {
+        textDecoration: "none",
+        color: "inherit"
       },
   });
 
@@ -29,11 +34,15 @@ const NavigationBottom = () => {
                             Our Catalog 
                         </Typography>
                         </Box>
-                        <Box p={2}>
+                        <Link to='/about-us' className={classes.a}>
+                      
+                            
                         <Typography variant="button" color="inherit" style={{ borderRight: '0.1em solid ', paddingRight: '33px' }}>
-                              Terms & Conditions 
+                              About Us
                         </Typography>
-                        </Box>
+                    
+                       
+                            </Link>
                         <Box p={2}>
                         <Typography variant="button" color="inherit">
                              Privacy
