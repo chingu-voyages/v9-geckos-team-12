738c320f79503136d039ee8a21f232ada92e6813
xdorijanx <dorijan11@gmail.com>
2019-07-14 23:03:21 +0200

    Reverted

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -163,8 +163,7 @@ const Basket = props => {
                       width: "200px",
                       padding: "10px",
                       boxShadow:
-                        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-                        margin: '0 auto',
+                        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
                     }}
                   >
                     <Box>
