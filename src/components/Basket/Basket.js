9dbb7c083246ba601091ab4bbd624fadead59163
xdorijanx <dorijan11@gmail.com>
2019-07-14 22:59:29 +0200

    Fixed order summary margin

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -163,7 +163,8 @@ const Basket = props => {
                       width: "200px",
                       padding: "10px",
                       boxShadow:
-                        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+                        margin: '0 auto',
                     }}
                   >
                     <Box>
